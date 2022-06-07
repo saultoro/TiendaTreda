adb364b909af1d34591cf36a2e7d04c784904977
saultoro <saultoro1104@gmail.com>
2022-06-07 09:42:05 -0500

    consulta SQL Ciudad Cliente

---
+++ Prueba SQL/consultaCiudadCliente.sql
@@ -0,0 +1,7 @@
+﻿
+select tblcliente.cedula, CONCAT(tblcliente.primer_nombre,tblcliente.primer_apellido), tblciudad.nombre,
+if(tblcliente.dias_mora BETWEEN 1 and 20, "Riesgo Bajo",if(tblcliente.dias_mora BETWEEN 21 and 25, "Riesgo Medio",if(tblcliente.dias_mora >35, "Riesgo Alto","Riesgo no identificado"))) AS riesgo
+FROM tblcliente INNER JOIN tblciudad ON
+tblciudad.id=tblcliente.id_ciudad
+ORDER BY tblcliente.dias_mora ASC
+
---
+++ Prueba SQL/consultaEstudiosPersona.sql
@@ -0,0 +1,5 @@
+﻿select tblestudios.Institucion,MAX(tblestudios.Fecha), tblpersona.CC, tblpersona.Nombre from tblestudios INNER JOIN
+tblpersona ON 
+tblpersona.CC=tblestudios.FKPersona
+GROUP by tblpersona.CC
+
